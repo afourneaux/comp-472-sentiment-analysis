9f49dbb7a9d3e9855c7b62d0411f48d1f3d0d03c
afourneaux <alex@fourneaux.ca>
2022-04-18 17:32:51 -0400

    Dependencies

--- source.py
+++ source.py
@@ -1,1 +1,22 @@
-import math
+import spacy
+import os
+from afinn import Afinn
+import csv
+
+nlp = spacy.load("en_core_web_sm")
+
+textFilePath = os.path.join(os.path.dirname(os.path.abspath(__file__)), "textfile.txt")
+file = open(textFilePath, "r")
+text = file.read()
+file.close()
+
+doc = nlp(text)
+afinn = Afinn()
+
+# for sentence in doc.sents:
+#     print(sentence)
+#     print(afinn.score(sentence))
+
+with open('output', "w") as outfile:
+    writer = csv.writer(outfile)
+    writer.writerow(["test", "row"])
